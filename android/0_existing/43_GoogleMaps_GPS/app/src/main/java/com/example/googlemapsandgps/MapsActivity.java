85503499c968c93e8a9f9cc00e55293b2981f7a2
GrizzlyMcBear <sronenco@gmail.com>
2022-02-01 23:57:44 +0200

    Updated several files: xml (layouts, manifest), java (activity) for the '43_GoogleMaps_GPS' application.

--- android/0_existing/43_GoogleMaps_GPS/app/src/main/java/com/example/googlemapsandgps/MapsActivity.java
+++ android/0_existing/43_GoogleMaps_GPS/app/src/main/java/com/example/googlemapsandgps/MapsActivity.java
@@ -1,22 +1,54 @@
 package com.example.googlemapsandgps;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.googlemapsandgps.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 	
 	private GoogleMap mMap;
 	private ActivityMapsBinding binding;
 	
+	//OnMapReadyCallback - interface that show the map when it ready.
+	TextView textView;
+	EditText editText, editText1,editText2;
+	Button button1;
+	String titleString, snippetString;
+	CheckBox checkBox;
+	boolean deleteMode = false;
+	
+	private boolean checkPermissions() {
+		return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+				ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+	}
+	
+	@SuppressLint("MissingPermission")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,8 +58,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 		
 		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
 		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.map);
-		mapFragment.getMapAsync(this);
+				.findFragmentById(R.id.map);	// Connect to the map (resource) fragment.
+		mapFragment.getMapAsync(this);	// Listener for when the map resource is ready to be used.
+		
+		// Ask for fine and coarse location permissions from the user.
+		ActivityCompat.requestPermissions(this,
+				new String[] { Manifest.permission.ACCESS_FINE_LOCATION,
+						Manifest.permission.ACCESS_COARSE_LOCATION},
+				1);
+		
+		LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		if (checkPermissions()) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+		
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1000, this);
+	}
+	
+	public void dialogCreator(LatLng latLng) {
+		final Dialog dialog = new Dialog(this);
+		dialog.setContentView(R.layout.dialog);
+		dialog.setCancelable(true);
+		
+		editText1 = dialog.findViewById(R.id.title);
+		editText2 = dialog.findViewById(R.id.snippet);
+		button1 = dialog.findViewById(R.id.btn1);
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				titleString = editText1.getText().toString();
+				snippetString = editText2.getText().toString();
+				Marker marker = mMap.addMarker(new MarkerOptions()
+						.position(latLng)
+						.title(titleString)
+						.snippet(snippetString));
+				dialog.dismiss();
+			}
+		});
+		dialog.show();
 	}
 	
 	/**
@@ -42,10 +117,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
 		mMap = googleMap;
+		textView = findViewById(R.id.text);
+		editText = findViewById(R.id.marker_name);
+		checkBox = findViewById(R.id.btn);
+		checkBox.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				deleteMode = !deleteMode;
+			}
+		});
 		
 		// Add a marker in Sydney and move the camera
-		LatLng sydney = new LatLng(-34, 151);
-		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+		LatLng specialLocation = new LatLng(32.9036454, 35.2322266);
+		mMap.addMarker(new MarkerOptions().position(specialLocation).title("Marker @ special location"));
+		mMap.moveCamera(CameraUpdateFactory.newLatLng(specialLocation));
+		
+		mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+			@Override
+			public void onMapClick(@NonNull LatLng latLng) {
+				dialogCreator(latLng);
+			}
+		});
+		
+		mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+			@Override
+			public boolean onMarkerClick(Marker marker) {
+				if (deleteMode) marker.remove();
+				else marker.showInfoWindow();
+				
+				return true;
+			}
+		});
+	}
+	
+	@Override
+	public void onLocationChanged(@NonNull Location location) {
+		
+		if(location!=null) {
+			LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+			//  mMap.addMarker(new MarkerOptions().position(myLocation).title("my location"));
+			mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+		}
+	}
+	
+	@Override
+	public void onLocationChanged(@NonNull List<Location> locations) {
+	}
+	
+	@Override
+	public void onFlushComplete(int requestCode) {
+	}
+	
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
+	
+	@Override
+	public void onProviderEnabled(@NonNull String provider) {
+	}
+	
+	@Override
+	public void onProviderDisabled(@NonNull String provider) {
+	}
+	
+	@Override
+	public void onPointerCaptureChanged(boolean hasCapture) {
 	}
 }

999f47154da660279c3d97d72491b2f8d058f782
GrizzlyMcBear <sronenco@gmail.com>
2022-02-01 20:16:30 +0200

    Added xml files (folder '.idea') for a new application ('36_ForegroundService').

---
+++ android/36_ForegroundService/app/src/main/java/com/example/foregroundservice/ForegroundService.java
@@ -0,0 +1,62 @@
+package com.example.foregroundservice;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class ForgeoundService extends Service {
+	
+	public ForgeoundService() {
+	}
+	
+	final int NOTIFY_ID = 1;
+	final int NOTIFY_ID1 = 2;
+	
+	/**
+	 * Not enabling other components to bind into this service.
+	 * @param intent
+	 * @return
+	 */
+	@Nullable
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		
+		Notification.Builder builder = new Notification.Builder(this);
+		builder.setSmallIcon(android.R.drawable.star_on).setContentTitle("Foreground service")
+				.setContentText("long lasting operation");
+		
+		Notification.Builder builder1 = new Notification.Builder(this);
+		builder1.setSmallIcon(android.R.drawable.star_on).setContentTitle("number 2")
+				.setContentText("long lasting operation");
+		
+		startForeground(NOTIFY_ID,builder.build());
+		
+		startForeground(NOTIFY_ID1,builder1.build());
+		
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				
+				stopForeground(true);
+			}
+		},5000);
+		
+		return super.onStartCommand(intent, flags, startId);
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Toast.makeText(this, "Inside onDestroy()", Toast.LENGTH_SHORT).show();
+	}
+}

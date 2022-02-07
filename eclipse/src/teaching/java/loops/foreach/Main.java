e92dffbb1e6d0edb7a075a8fc77c84da81c08c47
GrizzlyMcBear <sronenco@gmail.com>
2022-02-07 09:48:15 +0200

    Added examples explaining the 'foreach' loop.

---
+++ eclipse/src/teaching/java/loops/foreach/Main.java
@@ -0,0 +1,30 @@
+package teaching.java.loops.foreach;
+
+public class Main {
+
+	private static final int NUMBERS_AMOUNT = 10;
+	
+	public static void main(String[] args) {
+		example2();
+	}
+	
+	private static void example1() {
+		int[] numbers = new int[NUMBERS_AMOUNT];
+		
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = (i+1) * 2;
+		}
+		
+		for (int number : numbers) {
+			System.out.println(String.format("Curr multiplication of 2 is: %d", number));
+		}
+	}
+	
+	private static void example2() {
+		String[] words = new String[] { "teaching", "java", "loops", "foreach" };
+		
+		for (String currWord : words) {
+			System.out.println(String.format("Curr part of package name: %s", currWord));
+		}
+	}
+}

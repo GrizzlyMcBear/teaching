1c1590f6296cf3f32712590c3479ad6953f5e463
GrizzlyMcBear <sronenco@gmail.com>
2022-04-10 21:53:14 +0300

    Updated 2 applications: 1. The introduction app. 2. The 'Button' app, showing mainly the 3 types of adding a click listener to a widget.

--- android/0_topics/0_Introduction/0_BasicApp/app/src/main/java/com/example/basicapplication/MainActivity.java
+++ android/0_topics/0_Introduction/0_BasicApp/app/src/main/java/com/example/basicapplication/MainActivity.java
@@ -6,13 +6,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 	
+	/**
+	 * @implNote Connect the local fields to the layout's widgets.
+	 */
 	private void initLayout() {
 		setContentView(R.layout.activity_main);
 	}
 	
+	/**
+	 * This method gets called when an instance of this class is being created and before showing
+	 * the resulting "screen" to the user.
+	 * @param savedInstanceState
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);// Call this class's super 'onCreate' method.
 		initLayout();
 	}
 }
---
+++ android/0_topics/2_Widgets/0_Button/app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+	/**
+	 * The fields which refer 2 button widgets in the layout.
+	 * @implNote We define fields (properties) referring the button widgets,
+	 * allowing us to respond to user's actions and pass data in this activity's screen:
+	 * <ul>
+	 *     <li>A 'widget' is the UI component presented to the user.</li>
+	 *     <li>The widgets would be placed in the matching layout file (activity_main.xml).</li>
+	 * </ul>
+	 */
+	Button anonymousObjectButton;
+	Button implementInterfaceButton;
+	
+	private void initLayout() {
+		setContentView(R.layout.activity_main);
+		
+		anonymousObjectButton = findViewById(R.id.anonymousObjectButton);
+		implementInterfaceButton = findViewById(R.id.implementInterfaceButton);
+	}
+	/**
+	 * Calling this method would attach listeners to widgets in the layout,
+	 * using the fields referring them.
+	 * @implNote While this method only sets ("connects") a widget to the desired method,
+	 * the method would only be executed <u>when that specific event occurred</u>.
+	 * i.e. only after we set a {@link View#setOnClickListener(View.OnClickListener)} to the
+	 * `anonymousObjectButton` widget/field the following {@link #onClick(View)} method <u>could</u>
+	 * get called. It <u>would</u> get called however only after the user clicked that button.
+	 */
+	private void initWidgetsListeners() {
+		implementInterfaceButton.setOnClickListener(this);
+		anonymousObjectButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String message = "You've clicked the `Anonymous` button.";
+				Toast.makeText(v.getContext(), message, Toast.LENGTH_LONG).show();
+			}
+		});
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		initLayout();
+		initWidgetsListeners();
+	}
+	
+	/**
+	 * This method would get called each time a user would click a widget we've registered a listener
+	 * to its {@link View#setOnClickListener(View.OnClickListener)}.
+	 * @param view The object representing the widget which was clicked.
+	 */
+	@Override
+	public void onClick(View view) {
+		String message = "Hold on!!!!! What are you doing for god sake?!";
+		
+		if (view == implementInterfaceButton) {
+			message = "You've clicked the `Interface` button.";
+		}
+		
+		Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+	}
+	
+	//todo: continue refactoring this app from here.
+	public void attributeOnClick(View v) {
+		String message = "You've clicked the `Attribute` button.";
+		Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+		
+		// Create the text message with a string.
+		String textMessage = "This is my text message.";
+		Intent sendIntent = new Intent();
+		sendIntent.setAction(Intent.ACTION_SEND);
+		sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage);
+		sendIntent.setType("text/plain");
+		
+		// Try to invoke the intent.
+		try {
+			startActivity(sendIntent);
+		} catch (ActivityNotFoundException e) {
+			// Define what your app should do if no activity can handle the intent.
+		}
+	}
+}

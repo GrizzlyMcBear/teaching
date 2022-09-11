c5404b5846b09b47e914663d2370f236d2c1dada
GrizzlyMcBear <sronenco@gmail.com>
2022-09-11 10:02:29 +0300

    Added 3 new (small) programs: 1. A 'Hello World' to show the basics. 2. An output program to show the students how to print simple content on screen (Write v.s. WriteLine) 3. An input-reading program, getting the students familiar with `System.ReadLine` and the `Convert.ToInt32`

---
+++ VisualStudio/1_Basics/Introduction/Input/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input {
+	public class Program {
+		public static void Main(string[] args) {
+			ReadAndPrintInput();
+			ReadAndPrintManipulatedAge();
+		}
+
+		public static void ReadAndPrintInput() {
+			Console.Write("Please enter your input: ");
+			string userInput = Console.ReadLine();
+			Console.WriteLine("This is the user's input: " + userInput);
+		}
+
+		public static void ReadAndPrintManipulatedAge() {
+			Console.Write("Please enter your age: ");
+			string userInput = Console.ReadLine();
+			int userAge = Convert.ToInt32(userInput);
+
+			string userOutput = "Your age is: " + userAge;
+			Console.WriteLine(userOutput);
+
+			userOutput = String.Format("We can also duplicate your age ({0}) and get ({1})",
+				userAge, userAge * 2);//, userAge + 1);
+			Console.WriteLine(userOutput);
+		}
+	}
+}
---
+++ VisualStudio/1_Basics/Introduction/Output/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output {
+	public class Program {
+		public static void Main(string[] args) {
+			Console.WriteLine("This command prints text and descends 1 line.");
+			Console.Write("This command prints text and stays in the same line.");
+			Console.WriteLine();// We can also print nothing and descend 1 line
+
+			Console.WriteLine(7 + 8);// And also print the result of arithmetic expressions
+			Console.WriteLine("But see what happens when we combine text and arithmetic expression: " + 7 + 8);
+		}
+	}
+}

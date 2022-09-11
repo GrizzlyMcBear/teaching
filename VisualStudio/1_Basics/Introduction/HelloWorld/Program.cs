d77252dd08c1f5fd33ceadaa982221de70bf4318
GrizzlyMcBear <sronenco@gmail.com>
2022-09-11 12:20:14 +0300

    Added to the program: 1. Basic `WriteLine` 2. Existing comment types (both hebrew and english)

--- VisualStudio/1_Basics/Introduction/HelloWorld/Program.cs
+++ VisualStudio/1_Basics/Introduction/HelloWorld/Program.cs
@@ -5,8 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloWorld {
-	internal class Program {
-		static void Main(string[] args) {
+	public class Program {
+		public static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+
+			// This is a single-line comment
+			
+			// זוהי הערת-שורה בודדת
+
+			/* This is a
+			 * Multi-line comment
+			 * and continues untill reaching the
+			 * ending sign -> */
+
+			/* זוהי
+			 * הערה רב-שורתית
+			 * והיא נמשכת כל עוד לא כתבנו
+			 * את התו המסיים אותה והוא
+			 -> */
 		}
 	}
 }

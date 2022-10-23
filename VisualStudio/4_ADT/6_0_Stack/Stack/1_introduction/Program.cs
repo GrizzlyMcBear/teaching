9b7be9a552169871677d322240d339f4e617d923
GrizzlyMcBear <sronenco@gmail.com>
2022-10-23 12:46:45 +0300

    Added a project with example for an integers stack.

---
+++ VisualStudio/4_ADT/6_0_Stack/Stack/1_introduction/Program.cs
@@ -0,0 +1,15 @@
+﻿using _1_introduction.UsageExamples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_introduction {
+	public class Program {
+		public static void Main(string[] args) {
+
+			UsingNode.IntegersStack();
+		}
+	}
+}
---
+++ VisualStudio/4_ADT/6_0_Stack/Stack/1_introduction/UsageExamples/UsingNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADT;
+
+namespace _1_introduction.UsageExamples {
+	public class UsingNode {
+		public static void IntegersStack() {
+			ADT.Stack<int> intStack = new ADT.Stack<int>();
+
+			Console.WriteLine("intStack: " + intStack.ToString());
+			intStack.Push(4);
+
+			Console.WriteLine("intStack: " + intStack.ToString());
+			intStack.Push(3);
+			intStack.Push(10);
+
+			Console.WriteLine("intStack: " + intStack.ToString());
+
+			Console.WriteLine("Topmost stack item: " + intStack.Top());
+			Console.WriteLine("Popping out the topmost item.");
+			intStack.Pop();
+
+			Console.WriteLine("intStack: " + intStack.ToString());
+		}
+	}
+}
